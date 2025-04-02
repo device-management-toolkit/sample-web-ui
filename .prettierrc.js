221df10eee95d3e0c120d8058a1e2f40e9f16136
Mike <michael.johanson@intel.com>
2025-04-02 10:34:25 -0700

    refactor: apply html formatting

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,6 +2,7 @@ module.exports = {
   ...require('prettier-config-standard'),
   plugins: ['prettier-plugin-multiline-arrays'],
   printWidth: 120,
+  singleAttributePerLine: false,
   bracketSameLine: true,
   trailingComma: 'none',
   multilineArraysWrapThreshold: 2
