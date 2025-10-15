186f6cde0c613ef3397fbdbe49e5d6750c149c99
Madhavi Losetty <madhavi.losetty@intel.com>
2025-10-15 11:05:44 -0700

    build: removed unused references (#2929)

--- src/app/proxy-configs/proxy-config-detail/proxy-config-detail.component.ts
+++ src/app/proxy-configs/proxy-config-detail/proxy-config-detail.component.ts
@@ -15,7 +15,6 @@ import { ProxyConfigsService } from '../proxy-configs.service'
 import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card'
 import { MatError, MatFormField, MatLabel } from '@angular/material/form-field'
 import { MatInput } from '@angular/material/input'
-import { MatOption, MatSelect } from '@angular/material/select'
 import { MatButton } from '@angular/material/button'
 import { MatProgressBar } from '@angular/material/progress-bar'
 import { MatList, MatListItem } from '@angular/material/list'
@@ -36,8 +35,6 @@ import { MatIcon } from '@angular/material/icon'
     MatLabel,
     MatInput,
     MatError,
-    MatSelect,
-    MatOption,
     MatButton,
     MatProgressBar,
     MatList,
