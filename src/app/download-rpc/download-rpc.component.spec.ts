de1cb3087ece41644ff253508de9f17e028188a5
Mike Johanson <michael.johanson@intel.com>
2026-05-28 15:47:20 -0700

    feat(devices): add download-rpc component logic

---
+++ src/app/download-rpc/download-rpc.component.spec.ts
@@ -0,0 +1,203 @@
+/*********************************************************************
+ * Copyright (c) Intel Corporation 2022
+ * SPDX-License-Identifier: Apache-2.0
+ **********************************************************************/
+
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { of, throwError } from 'rxjs'
+import { TranslateModule } from '@ngx-translate/core'
+import { provideHttpClient } from '@angular/common/http'
+import { provideHttpClientTesting } from '@angular/common/http/testing'
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { DownloadRpcComponent } from './download-rpc.component'
+import { DownloadRpcService } from './download-rpc.service'
+import { ProfilesService } from '../profiles/profiles.service'
+import { DomainsService } from '../domains/domains.service'
+import { RpcRelease } from './download-rpc.constants'
+
+describe('DownloadRpcComponent', () => {
+  let component: DownloadRpcComponent
+  let fixture: ComponentFixture<DownloadRpcComponent>
+  let downloadServiceSpy: jasmine.SpyObj<DownloadRpcService>
+  let profilesServiceSpy: jasmine.SpyObj<ProfilesService>
+  let domainsServiceSpy: jasmine.SpyObj<DomainsService>
+  let snackBarSpy: jasmine.SpyObj<MatSnackBar>
+
+  const releases: RpcRelease[] = [
+    { version: 'v3.0.1', assets: [{ os: 'linux', arch: 'x86_64' }, { os: 'windows', arch: 'x86_64' }] }
+  ]
+
+  beforeEach(() => {
+    downloadServiceSpy = jasmine.createSpyObj('DownloadRpcService', ['getVersions', 'buildPackage'])
+    profilesServiceSpy = jasmine.createSpyObj('ProfilesService', ['getData'])
+    domainsServiceSpy = jasmine.createSpyObj('DomainsService', ['getData'])
+    snackBarSpy = jasmine.createSpyObj('MatSnackBar', ['open'])
+    downloadServiceSpy.getVersions.and.returnValue(of(releases))
+    downloadServiceSpy.buildPackage.and.returnValue(of(new Blob(['zip'], { type: 'application/zip' })))
+    profilesServiceSpy.getData.and.returnValue(
+      of({
+        data: [
+          { profileName: 'acmProfile', activation: 'acmactivate' } as any,
+          { profileName: 'ccmProfile', activation: 'ccmactivate' } as any
+        ],
+        totalCount: 2
+      })
+    )
+    domainsServiceSpy.getData.and.returnValue(
+      of({ data: [{ profileName: 'dom1', domainSuffix: 'd.com' } as any], totalCount: 1 })
+    )
+
+    TestBed.configureTestingModule({
+      imports: [BrowserAnimationsModule, DownloadRpcComponent, TranslateModule.forRoot()],
+      providers: [
+        { provide: DownloadRpcService, useValue: downloadServiceSpy },
+        { provide: ProfilesService, useValue: profilesServiceSpy },
+        { provide: DomainsService, useValue: domainsServiceSpy },
+        { provide: MatSnackBar, useValue: snackBarSpy },
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    })
+    fixture = TestBed.createComponent(DownloadRpcComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it('should create and load versions, profiles, domains', () => {
+    expect(component).toBeTruthy()
+    expect(downloadServiceSpy.getVersions).toHaveBeenCalled()
+    expect(profilesServiceSpy.getData).toHaveBeenCalled()
+    expect(domainsServiceSpy.getData).toHaveBeenCalled()
+    expect(component.releases.length).toBe(1)
+  })
+
+  it('defaults command to activate and auth to token', () => {
+    expect(component.form.get('command')?.value).toBe('activate')
+    expect(component.form.get('authMode')?.value).toBe('token')
+  })
+
+  it('requires username/password only when auth is userpass', () => {
+    component.form.get('authMode')?.setValue('token')
+    component.onAuthModeChange()
+    expect(component.form.get('username')?.hasError('required')).toBeFalse()
+    component.form.get('authMode')?.setValue('userpass')
+    component.onAuthModeChange()
+    expect(component.form.get('username')?.hasError('required')).toBeTrue()
+    expect(component.form.get('password')?.hasError('required')).toBeTrue()
+  })
+
+  it('requires domain only when activate + ACM profile selected', () => {
+    component.form.get('command')?.setValue('activate')
+    component.form.get('profile')?.setValue('ccmProfile')
+    component.onProfileOrCommandChange()
+    expect(component.isAcmSelected).toBeFalse()
+    expect(component.form.get('domain')?.hasError('required')).toBeFalse()
+
+    component.form.get('profile')?.setValue('acmProfile')
+    component.onProfileOrCommandChange()
+    expect(component.isAcmSelected).toBeTrue()
+    expect(component.form.get('domain')?.hasError('required')).toBeTrue()
+  })
+
+  it('clears profile/domain requirements for deactivate', () => {
+    component.form.get('command')?.setValue('deactivate')
+    component.onProfileOrCommandChange()
+    expect(component.form.get('profile')?.hasError('required')).toBeFalse()
+    expect(component.form.get('domain')?.hasError('required')).toBeFalse()
+  })
+
+  it('selectedRelease exposes assets for the chosen version', () => {
+    component.form.get('version')?.setValue('v3.0.1')
+    component.onVersionChange()
+    expect(component.availableAssets.length).toBe(2)
+  })
+
+  it('onSubmit posts a token activate request and triggers a download', () => {
+    const anchorSpy = document.createElement('a')
+    spyOn(anchorSpy, 'click')
+    spyOn(document, 'createElement').and.returnValue(anchorSpy as any)
+    spyOn(window.URL, 'createObjectURL').and.returnValue('blob:url')
+    spyOn(window.URL, 'revokeObjectURL')
+
+    component.form.setValue({
+      command: 'activate',
+      version: 'v3.0.1',
+      os: 'linux',
+      arch: 'x86_64',
+      authMode: 'token',
+      username: '',
+      password: '',
+      profile: 'ccmProfile',
+      domain: ''
+    })
+    component.onProfileOrCommandChange()
+    component.onSubmit()
+
+    expect(downloadServiceSpy.buildPackage).toHaveBeenCalledWith({
+      command: 'activate',
+      version: 'v3.0.1',
+      os: 'linux',
+      arch: 'x86_64',
+      auth: { mode: 'token' },
+      profile: 'ccmProfile'
+    })
+    expect(anchorSpy.click).toHaveBeenCalled()
+  })
+
+  it('onSubmit includes domain for ACM and username/password for userpass', () => {
+    spyOn(component as any, 'saveBlob')
+    component.form.setValue({
+      command: 'activate',
+      version: 'v3.0.1',
+      os: 'linux',
+      arch: 'x86_64',
+      authMode: 'userpass',
+      username: 'admin',
+      password: 'secret',
+      profile: 'acmProfile',
+      domain: 'dom1'
+    })
+    component.onAuthModeChange()
+    component.onProfileOrCommandChange()
+    component.onSubmit()
+
+    expect(downloadServiceSpy.buildPackage).toHaveBeenCalledWith({
+      command: 'activate',
+      version: 'v3.0.1',
+      os: 'linux',
+      arch: 'x86_64',
+      auth: { mode: 'userpass', username: 'admin', password: 'secret' },
+      profile: 'acmProfile',
+      domain: 'dom1'
+    })
+  })
+
+  it('does not submit an invalid form', () => {
+    component.form.get('version')?.setValue('')
+    component.form.get('command')?.setValue('activate')
+    component.onProfileOrCommandChange()
+    component.onSubmit()
+    expect(downloadServiceSpy.buildPackage).not.toHaveBeenCalled()
+  })
+
+  it('shows an error when buildPackage fails', () => {
+    downloadServiceSpy.buildPackage.and.returnValue(throwError(() => ['fail']))
+    snackBarSpy.open.calls.reset()
+    component.form.setValue({
+      command: 'deactivate',
+      version: 'v3.0.1',
+      os: 'linux',
+      arch: 'x86_64',
+      authMode: 'token',
+      username: '',
+      password: '',
+      profile: '',
+      domain: ''
+    })
+    component.onAuthModeChange()
+    component.onProfileOrCommandChange()
+    component.onSubmit()
+    expect(snackBarSpy.open).toHaveBeenCalled()
+  })
+})
---
+++ src/app/download-rpc/download-rpc.component.ts
@@ -0,0 +1,210 @@
+/*********************************************************************
+ * Copyright (c) Intel Corporation 2022
+ * SPDX-License-Identifier: Apache-2.0
+ **********************************************************************/
+
+import { Component, OnInit, inject } from '@angular/core'
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms'
+import { MatButton } from '@angular/material/button'
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card'
+import { MatFormField, MatLabel, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field'
+import { MatInput } from '@angular/material/input'
+import { MatSelect } from '@angular/material/select'
+import { MatOption } from '@angular/material/core'
+import { MatRadioButton, MatRadioGroup } from '@angular/material/radio'
+import { MatProgressBar } from '@angular/material/progress-bar'
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { TranslateModule, TranslateService } from '@ngx-translate/core'
+import { finalize } from 'rxjs/operators'
+import { ProfilesService } from '../profiles/profiles.service'
+import { DomainsService } from '../domains/domains.service'
+import { Profile, ACM_ACTIVATION } from '../profiles/profiles.constants'
+import { Domain } from '../../models/models'
+import SnackbarDefaults from '../shared/config/snackBarDefault'
+import { DownloadRpcService } from './download-rpc.service'
+import {
+  AuthMode,
+  AuthModes,
+  PackageRequest,
+  RpcAsset,
+  RpcCommand,
+  RpcCommands,
+  RpcRelease,
+  detectOS
+} from './download-rpc.constants'
+
+@Component({
+  selector: 'app-download-rpc',
+  templateUrl: './download-rpc.component.html',
+  styleUrl: './download-rpc.component.scss',
+  imports: [
+    ReactiveFormsModule,
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatSelect,
+    MatOption,
+    MatRadioGroup,
+    MatRadioButton,
+    MatButton,
+    MatProgressBar,
+    TranslateModule
+  ],
+  providers: [{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { subscriptSizing: 'dynamic' } }]
+})
+export class DownloadRpcComponent implements OnInit {
+  private readonly fb = inject(FormBuilder)
+  private readonly downloadService = inject(DownloadRpcService)
+  private readonly profilesService = inject(ProfilesService)
+  private readonly domainsService = inject(DomainsService)
+  private readonly snackBar = inject(MatSnackBar)
+  private readonly translate = inject(TranslateService)
+
+  public readonly commands = RpcCommands
+  public readonly authMethods = AuthModes
+
+  public releases: RpcRelease[] = []
+  public profiles: Profile[] = []
+  public domains: Domain[] = []
+  public availableAssets: RpcAsset[] = []
+  public isAcmSelected = false
+  public isLoading = false
+
+  public form: FormGroup = this.fb.group({
+    command: ['activate' as RpcCommand, Validators.required],
+    version: ['', Validators.required],
+    os: ['', Validators.required],
+    arch: ['', Validators.required],
+    authMode: ['token' as AuthMode, Validators.required],
+    username: [''],
+    password: [''],
+    profile: [''],
+    domain: ['']
+  })
+
+  ngOnInit(): void {
+    this.downloadService.getVersions().subscribe({
+      next: (releases) => {
+        this.releases = releases
+      },
+      error: () => this.showError('downloadRpc.failVersions.value')
+    })
+    this.profilesService.getData().subscribe({
+      next: (res) => {
+        this.profiles = res.data
+      }
+    })
+    this.domainsService.getData().subscribe({
+      next: (res) => {
+        this.domains = res.data
+      }
+    })
+    this.onProfileOrCommandChange()
+  }
+
+  onVersionChange(): void {
+    const version = this.form.get('version')?.value
+    const release = this.releases.find((r) => r.version === version)
+    this.availableAssets = release ? release.assets : []
+    const detected = detectOS()
+    const match = this.availableAssets.find((a) => a.os === detected) ?? this.availableAssets[0]
+    if (match) {
+      this.form.get('os')?.setValue(match.os)
+      this.form.get('arch')?.setValue(match.arch)
+    }
+  }
+
+  onAuthModeChange(): void {
+    const userpass = this.form.get('authMode')?.value === 'userpass'
+    const username = this.form.get('username')
+    const password = this.form.get('password')
+    if (userpass) {
+      username?.setValidators([Validators.required])
+      password?.setValidators([Validators.required])
+    } else {
+      username?.clearValidators()
+      password?.clearValidators()
+    }
+    username?.updateValueAndValidity()
+    password?.updateValueAndValidity()
+  }
+
+  onProfileOrCommandChange(): void {
+    const isActivate = this.form.get('command')?.value === 'activate'
+    const profileCtrl = this.form.get('profile')
+    const domainCtrl = this.form.get('domain')
+
+    if (isActivate) {
+      profileCtrl?.setValidators([Validators.required])
+    } else {
+      profileCtrl?.clearValidators()
+    }
+
+    const selected = this.profiles.find((p) => p.profileName === profileCtrl?.value)
+    this.isAcmSelected = isActivate && selected?.activation === ACM_ACTIVATION
+
+    if (this.isAcmSelected) {
+      domainCtrl?.setValidators([Validators.required])
+    } else {
+      domainCtrl?.clearValidators()
+    }
+    profileCtrl?.updateValueAndValidity()
+    domainCtrl?.updateValueAndValidity()
+  }
+
+  onSubmit(): void {
+    if (this.form.invalid) {
+      return
+    }
+    const v = this.form.value
+    const request: PackageRequest = {
+      command: v.command,
+      version: v.version,
+      os: v.os,
+      arch: v.arch,
+      auth:
+        v.authMode === 'userpass'
+          ? { mode: 'userpass', username: v.username, password: v.password }
+          : { mode: 'token' }
+    }
+    if (v.command === 'activate') {
+      request.profile = v.profile
+      if (this.isAcmSelected) {
+        request.domain = v.domain
+      }
+    }
+
+    this.isLoading = true
+    this.downloadService
+      .buildPackage(request)
+      .pipe(finalize(() => (this.isLoading = false)))
+      .subscribe({
+        next: (blob) => {
+          this.saveBlob(blob, `rpc-${request.command}-${request.os}-${request.arch}.zip`)
+          this.snackBar.open(
+            this.translate.instant('downloadRpc.success.value'),
+            undefined,
+            SnackbarDefaults.defaultSuccess
+          )
+        },
+        error: () => this.showError('downloadRpc.failPackage.value')
+      })
+  }
+
+  private saveBlob(blob: Blob, filename: string): void {
+    const url = window.URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.href = url
+    a.download = filename
+    a.click()
+    window.URL.revokeObjectURL(url)
+  }
+
+  private showError(key: string): void {
+    this.snackBar.open(this.translate.instant(key), undefined, SnackbarDefaults.defaultError)
+  }
+}
